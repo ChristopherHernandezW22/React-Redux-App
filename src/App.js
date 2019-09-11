2dccd825f7e6f55ab18c18727a2822db563f2eb7
Christopher Hernandez <cerberean661@gmail.com>
2019-09-11 14:21:38 -0700

    fetch information retrieved on console

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
+
+const apiCall = () =>{
+
+  fetch("https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "omgvamp-hearthstone-v1.p.rapidapi.com",
+		"x-rapidapi-key": "526395db8emshdb6e39fb34ce32bp162132jsn16ecdb8b1bee"
+	}
+})
+.then(response => {
+	return response.json();
+})
+.then(res=>{
+  console.log(res);
+})
+.catch(err => {
+	console.log("error", err);
+});
+}
 
 function App() {
   return (
     <div className="App">
+      <button onClick={()=>apiCall()}>Fetch call</button>
       <h1>Hearthstone API</h1>
     </div>
   );
--- src/actions/Action.js
+++ src/actions/Action.js
@@ -1,3 +1,14 @@
+import axios from "axios";
+import { dispatch } from "rxjs/internal/observable/range";
+
 const FETCH_CARDID_START = "FETCH_CARDID_START";
 const FETCH_CARDID_SUCCESS = "FETCH_CARDID_SUCCESS";
 const FETCH_CARDID_FAIL = "FETCH_CARDID_FAIL";
+
+const getCardID = () => dispatch => {
+    dispatch({ type: FETCH_CARDID_START });
+    axios.get("https://rapidapi.com/omgvamp/api/hearthstone?apiKey=526395db8emshdb6e39fb34ce32bp162132jsn16ecdb8b1bee")
+    .then (res => console.log(res))
+    .catch (err => console.log(err))
+
+};
